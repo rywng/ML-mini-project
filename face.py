c340a4cc89c803c32ac52c5033b58fd03c5d0fdd
Ryan <ryan@alien.gov>
2023-12-10 18:40:34 +0800

    WIP: resnet

--- face.py
+++ face.py
@@ -1,11 +1,10 @@
-from datetime import date
 import argparse
 
 import torch
 from torch.utils.tensorboard.writer import SummaryWriter
 
 from logger_utils import plot_random_batch
-from models import SmilingClassifier, train_model
+from models import Resnet
 from preprocessing import get_dataloaders
 
 TEST_SIZE = 0.3
@@ -16,39 +15,38 @@ BATCH_SIZE = 64
 def main(args):
     # Check command-line arguments
 
-    if torch.cuda.is_available() and not args["no-cuda"]:
+    if torch.cuda.is_available() and not args.nocuda:
         dev = "cuda"
     else:
         dev = "cpu"
 
     train_dataloader, test_dataloader = get_dataloaders(
-        args["dataset_location"], dev)
+        args.dataset_location, dev)
 
     writer = SummaryWriter('runs/face-smile')
 
     writer.add_figure("One batch",
                       plot_random_batch(train_dataloader, BATCH_SIZE))
     # Train model
-    model = train_model(SmilingClassifier(), train_dataloader, test_dataloader,
-                        300, dev, writer)
-
-    # Save model to file
-    if args["save"]:
-        filename = f"{date.today()}.pt"
-        torch.save(model.state_dict(), filename)
-        print(f"Model saved to {filename}.")
+    # model = train_model(SmilingClassifier(), train_dataloader, test_dataloader,
+    #                     300, dev, writer)
+    net = Resnet()
+    net.train_model(train_dataloader,
+                    test_dataloader,
+                    int(args.epochs),
+                    dev,
+                    writer,
+                    name="resnet50")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="pipeline",
                                      description="Train the model")
     parser.add_argument("dataset_location")
-    parser.add_argument("-s",
-                        "--save",
-                        action="store_true",
-                        help="save the model")
+    parser.add_argument("-e", "--epochs", default="300")
     parser.add_argument("--no-cuda",
                         action="store_true",
+                        dest="nocuda",
                         help="Don't use cuda for training")
 
     args = parser.parse_args()
