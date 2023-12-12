29e1a8ee508b7f4834bf5dcd8c1f680ff39ab381
Ryan <ryan@alien.gov>
2023-12-12 19:14:29 +0800

    Update logger

--- logger_utils.py
+++ logger_utils.py
@@ -3,7 +3,6 @@ import numpy as np
 from torch.functional import Tensor
 from torch.utils.data import DataLoader
 
-
 FIGSIZE = (12, 12)
 
 
@@ -11,7 +10,7 @@ def matplotlib_imshow(img, one_channel=False):
     img = Tensor.cpu(img)
     if one_channel:
         img = img.mean(dim=0)
-    img = img / 2 + 0.5  # unnormalize
+    # img = img / 2 + 0.5  # unnormalize
     npimg = img.numpy()
     if one_channel:
         plt.imshow(npimg, cmap="Greys")
@@ -20,22 +19,26 @@ def matplotlib_imshow(img, one_channel=False):
 
 
 def plot_classes_preds(net, images, labels):
-    '''
-    Generates matplotlib Figure using a trained network, along with images
-    and labels from a batch, that shows the network's top prediction along
-    with its probability, alongside the actual label, coloring this
-    information based on whether the prediction was correct or not.
+    """Generates matplotlib Figure using a trained network, along with images
+    and labels from a batch, that shows the network's top prediction along with
+    its probability, alongside the actual label, coloring this information
+    based on whether the prediction was correct or not.
+
     Uses the "images_to_probs" function.
-    '''
-    RANGE = min(16, len(labels))
-    ROWS = 4
+    """
+    RANGE = min(12, len(labels))
+    ROWS = 3
     images = images[:RANGE]
     labels = labels[:RANGE]
     probs = net(images)
     # plot the images in the batch, along with predicted and true labels
     fig = plt.figure(figsize=FIGSIZE)
     for idx in np.arange(RANGE):
-        ax = fig.add_subplot(ROWS, RANGE // ROWS, idx + 1, xticks=[], yticks=[])
+        ax = fig.add_subplot(ROWS,
+                             RANGE // ROWS,
+                             idx + 1,
+                             xticks=[],
+                             yticks=[])
         matplotlib_imshow(images[idx], one_channel=False)
         ax.set_title(
             f"{float(probs[idx] * 100.0):.2f}%, {float(labels[idx] * 100):.2f}%\n"
